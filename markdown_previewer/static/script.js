94841865c48ab90cf254fc75213495168400a1fc
ManognaMandaloju26 <manognamandaloju@gmail.com>
2025-05-29 16:14:16 +0530

    Add files via upload

---
+++ markdown_previewer/static/script.js
@@ -0,0 +1,8 @@
+
+const input = document.getElementById('markdown-input');
+const preview = document.getElementById('preview');
+
+input.addEventListener('input', () => {
+    const markdownText = input.value;
+    preview.innerHTML = marked.parse(markdownText);
+});
